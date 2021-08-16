58c044fd46bcb50becd1288be25c4211ce640304
shinoburc <dandydot@gmail.com>
2021-08-16 16:44:51 +0900

    add bootstrap.

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 
+/* Bootstrap */
+import 'bootstrap/dist/css/bootstrap.css'
+
 createApp(App).mount('#app')
