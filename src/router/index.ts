f8511309d426bd7d29406f85e4bfef45354b79d2
shinoburc <dandydot@gmail.com>
2021-08-30 15:00:18 +0900

    fix for electron.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import { createRouter, createWebHashHistory, createWebHistory, RouteRecordRaw } from 'vue-router'
 import Home from '../views/Home.vue'
 import ApplicationHeader from '../components/ApplicationHeader.vue'
 
@@ -19,7 +19,9 @@ const routes: Array<RouteRecordRaw> = [
     // which is lazy-loaded when the route is visited.
     components: {
       application_header: ApplicationHeader,
-      content: import(/* webpackChunkName: "about" */ '../views/InputForm.vue')
+      //content: import(/* webpackChunkName: "about" */ '@/views/InputForm.vue')
+      // for electron
+      content: () => import('@/views/InputForm.vue')
     }
   },
   {
@@ -27,48 +29,49 @@ const routes: Array<RouteRecordRaw> = [
     name: 'Printing',
     components: {
       application_header: ApplicationHeader,
-      printing: import('../views/Printing.vue')
+      printing: () => import('@/views/Printing.vue')
     }
   },
   {
     path: '/report_A4',
     name: 'A4',
     components: {
-      printing: import('../reports/A4.vue')
+      printing: () => import('@/reports/A4.vue')
     }
   },
   {
     path: '/report_A4_Landscape',
     name: 'A4Landscape',
     components: {
-      printing: import('../reports/A4Landscape.vue')
+      printing: () => import('@/reports/A4Landscape.vue')
     }
   },
   {
     path: '/report_A5',
     name: 'A5',
     components: {
-      printing: import('../reports/A5.vue')
+      printing: () => import('@/reports/A5.vue')
     }
   },
   {
     path: '/report_Invoice',
     name: 'Invoice',
     components: {
-      printing: import('../reports/Invoice.vue')
+      printing: () => import('@/reports/Invoice.vue')
     }
   },
   {
     path: '/report_Flexible',
     name: 'Flexible',
     components: {
-      printing: import('../reports/Flexible.vue')
+      printing: () => import('@/reports/Flexible.vue')
     }
   }
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  //history: createWebHistory(process.env.BASE_URL),
+  history: process.env.IS_ELECTRON ? createWebHashHistory(process.env.BASE_URL) : createWebHistory(process.env.BASE_URL),
   routes
 })
 
